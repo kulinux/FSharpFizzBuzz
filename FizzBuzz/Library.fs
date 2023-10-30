9d795dbc4fc1449725efab2379c5bea1597a85d0
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2023-10-30 22:35:30 +0100

    More explicit

--- FizzBuzz/Library.fs
+++ FizzBuzz/Library.fs
@@ -1,23 +1,19 @@
 ﻿namespace FizzBuzz
 
-
 type FizzBuzzType =
     | Number of int
     | Fizz
     | Buzz
     | FizzBuzz
 
-
-
-
-
 module FizzBuzz =
     let IsDivisibleBy div number = number % div = 0
     let IsDivisibleByThree = IsDivisibleBy 3
     let IsDivisibleByFive = IsDivisibleBy 5
+    let IsDivisibleByThreeAndFive number = IsDivisibleByThree number && IsDivisibleByFive number
 
     let FizzBuzz number : FizzBuzzType =
-        if IsDivisibleByThree number && IsDivisibleByFive number then FizzBuzz
+        if IsDivisibleByThreeAndFive number then FizzBuzz
         elif IsDivisibleByThree number then Fizz
         elif IsDivisibleByFive number then Buzz
         else Number number
